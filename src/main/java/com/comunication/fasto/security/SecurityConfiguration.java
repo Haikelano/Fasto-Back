0f60563efa7ff15b5e03a3b5ba6749e029632c2b
Haykel Maaoui <haykel.maaoui@gmail.com>
2018-11-30 17:52:21 +0100

    a terminer les simple autherization

--- src/main/java/com/comunication/fasto/security/SecurityConfiguration.java
+++ src/main/java/com/comunication/fasto/security/SecurityConfiguration.java
@@ -7,32 +7,51 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableConfigurationProperties
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     MongoUserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private PasswordEncoder bCryptPasswordEncoder() {
+        return  new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement().disable();
-    }
-
+        http.
+                authorizeRequests()
+                .antMatchers("/static/**", "/registration","/login").permitAll()
+                .antMatchers("/users/**").authenticated()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated()
+                .and().csrf().disable().httpBasic().disable()
+                .formLogin().defaultSuccessUrl("/")
+                .permitAll().usernameParameter("login")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                .permitAll(); }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+
+    /*
     @Override
     public void configure(AuthenticationManagerBuilder builder) throws Exception {
         builder.userDetailsService(userDetailsService);
-    }
+    }*/
 }
